952611871a2661c8cb961811ac082e458a86d748
Sean Kelly <sean.kelly@faraday.io>
2018-09-27 09:45:52 -0400

    Visual

---
+++ nass.R
@@ -0,0 +1,12 @@
+#' Inspect NASS data
+#' 
+
+library(tidyverse)
+
+data = read.csv('All Public Maple Syrup Data.csv') %>%
+       filter(Program == 'SURVEY' & Geo.Level == 'STATE') %>%
+       filter(Data.Item == 'MAPLE SYRUP - YIELD, MEASURED IN GALLONS / TAP') %>%
+       mutate(x = Data.Item)
+
+
+ggplot(data, aes(x=Year, y=Value, group=State, color=State)) + geom_line()
