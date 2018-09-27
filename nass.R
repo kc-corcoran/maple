2ecca3530a3a7a8b8caa27d23c55f16bdc01d1b4
Sean Kelly <sean.kelly@faraday.io>
2018-09-27 09:48:07 -0400

    Mutate not needed

--- nass.R
+++ nass.R
@@ -5,8 +5,7 @@ library(tidyverse)
 
 data = read.csv('All Public Maple Syrup Data.csv') %>%
        filter(Program == 'SURVEY' & Geo.Level == 'STATE') %>%
-       filter(Data.Item == 'MAPLE SYRUP - YIELD, MEASURED IN GALLONS / TAP') %>%
-       mutate(x = Data.Item)
+       filter(Data.Item == 'MAPLE SYRUP - YIELD, MEASURED IN GALLONS / TAP')
 
 
 ggplot(data, aes(x=Year, y=Value, group=State, color=State)) + geom_line()
